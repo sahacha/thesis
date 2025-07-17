0c2a90578668878bb2894813081203bb310f09a7
Sahacha <castby9weipage@gmail.com>
2025-07-18 02:43:42 +0700

    fix: delete old demo streamlit frontend, update new demo and add streamlit pip

---
+++ frontend/demo_light/demo_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+st.set_page_config("Chiang Mai Q&A Travel & Planing", "https://e-cms.rmutl.ac.th/assets/upload/images/2017/06/post_thumbnail_2017060611262952915.jpg")
+st.logo("https://e-cms.rmutl.ac.th/assets/upload/images/2017/06/post_thumbnail_2017060611262952915.jpg")
+st.title("Chiang Mai Q&A Travel & Planing")
+st.html(
+    f"""
+    <style>
+    body {{
+        -webkit-font-smoothing: antialiased;
+    }}
+    </style>
+    """
+)
+st.chat_input()
+    
+st.markdown("""
+ """)
