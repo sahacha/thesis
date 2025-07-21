1c36e2408db5ecbc6d04c71b2a0dce93d431154c
Sahacha <castby9weipage@gmail.com>
2025-07-21 22:31:56 +0700

    change path and fix demo_app.py

---
+++ frontend/demo_light/chat_rag.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.title("Chiang Mai Q&A Travel & Planing")
+st.write("Welcome to the Chiang Mai Q&A Travel & Planning app!")
+st.chat_input()
--- frontend/demo_light/demo_app.py
+++ frontend/demo_light/demo_app.py
@@ -1,17 +1,24 @@
 import streamlit as st
 st.set_page_config("Chiang Mai Q&A Travel & Planing", "https://e-cms.rmutl.ac.th/assets/upload/images/2017/06/post_thumbnail_2017060611262952915.jpg")
 st.logo("https://e-cms.rmutl.ac.th/assets/upload/images/2017/06/post_thumbnail_2017060611262952915.jpg")
-st.title("Chiang Mai Q&A Travel & Planing")
+import warnings
+warnings.filterwarnings("ignore")
+chat_history = []
+
+
 st.html(
     f"""
     <style>
-    body {{
+    body {{http://localhost:8501/
         -webkit-font-smoothing: antialiased;
     }}
     </style>
     """
 )
-st.chat_input()
-    
-st.markdown("""
- """)
+
+chat_rag = st.Page("chat_rag.py", title="Chiang Mai Q&A Travel & Planing")
+chat_history = st.Page("tools/history.py", title="Chat History")
+pg = st.navigation(
+    [chat_rag, chat_history]
+)
+pg.run()
