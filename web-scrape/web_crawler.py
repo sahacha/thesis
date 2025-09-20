c527abbd0adf5ff7467481f0a2e609125a3ba6a0
Donne Martin <castby9weipage@gmail.com>
2025-09-20 17:08:47 +0700

    del: not using file

--- web-scrape/web_crawler.py
+++
@@ -1,6 +0,0 @@
-import asyncio
-from crawl4ai import AsyncWebCrawler
-async def main():
-    async with AsyncWebCrawler() as crawler:
-        documents = await crawler.crawl_wongnai_places()
-        return documents
